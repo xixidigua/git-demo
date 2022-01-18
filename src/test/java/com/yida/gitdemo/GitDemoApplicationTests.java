8633f9638ab848805dd50485074b29e585e63185
xixidiga <38640170@qq.com>
2022-01-18 09:30:34 +0800

    提交了新工程

---
+++ src/test/java/com/yida/gitdemo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yida.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
