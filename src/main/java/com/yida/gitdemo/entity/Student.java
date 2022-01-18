81266dc9f57f9e7afe45d9ec63722a3e0211be6c
xixidiga <38640170@qq.com>
2022-01-18 10:06:46 +0800

    新增了entity

---
+++ src/main/java/com/yida/gitdemo/entity/Student.java
@@ -0,0 +1,40 @@
+package com.yida.gitdemo.entity;
+
+public class Student {
+    private Integer id;
+    private  String name;
+    private  String sex;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
