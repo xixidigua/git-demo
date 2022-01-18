4ac95cfe0a2d56d6f2f22c403fbb354cd6ad8e35
xixidiga <38640170@qq.com>
2022-01-18 09:46:56 +0800

    新增了文件

--- src/main/java/com/yida/gitdemo/controller/IndexController.java
+++ src/main/java/com/yida/gitdemo/controller/IndexController.java
@@ -1,4 +1,12 @@
 package com.yida.gitdemo.controller;
 
-public class IndexController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //RestController 加了Rest后返回前端的是字符串 要不就被thmeleaf解析
+public class IndexController  {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
 }
